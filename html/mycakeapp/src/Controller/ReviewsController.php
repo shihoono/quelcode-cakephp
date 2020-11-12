ff6ee907ddcba1fab7cab1aef71b6a156656c105
shihoono <shiho1449@gmail.com>
2020-11-12 15:09:54 +0900

    Reviewsのコントローラーファイル作成

---
+++ html/mycakeapp/src/Controller/ReviewsController.php
@@ -0,0 +1,115 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+/**
+ * Reviews Controller
+ *
+ * @property \App\Model\Table\ReviewsTable $Reviews
+ *
+ * @method \App\Model\Entity\Review[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
+ */
+class ReviewsController extends AppController
+{
+    /**
+     * Index method
+     *
+     * @return \Cake\Http\Response|null
+     */
+    public function index()
+    {
+        $this->paginate = [
+            'contain' => ['Reviewers', 'Reviewees', 'Bidinfos'],
+        ];
+        $reviews = $this->paginate($this->Reviews);
+
+        $this->set(compact('reviews'));
+    }
+
+    /**
+     * View method
+     *
+     * @param string|null $id Review id.
+     * @return \Cake\Http\Response|null
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function view($id = null)
+    {
+        $review = $this->Reviews->get($id, [
+            'contain' => ['Reviewers', 'Reviewees', 'Bidinfos'],
+        ]);
+
+        $this->set('review', $review);
+    }
+
+    /**
+     * Add method
+     *
+     * @return \Cake\Http\Response|null Redirects on successful add, renders view otherwise.
+     */
+    public function add()
+    {
+        $review = $this->Reviews->newEntity();
+        if ($this->request->is('post')) {
+            $review = $this->Reviews->patchEntity($review, $this->request->getData());
+            if ($this->Reviews->save($review)) {
+                $this->Flash->success(__('The review has been saved.'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('The review could not be saved. Please, try again.'));
+        }
+        $reviewers = $this->Reviews->Reviewers->find('list', ['limit' => 200]);
+        $reviewees = $this->Reviews->Reviewees->find('list', ['limit' => 200]);
+        $bidinfos = $this->Reviews->Bidinfos->find('list', ['limit' => 200]);
+        $this->set(compact('review', 'reviewers', 'reviewees', 'bidinfos'));
+    }
+
+    /**
+     * Edit method
+     *
+     * @param string|null $id Review id.
+     * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function edit($id = null)
+    {
+        $review = $this->Reviews->get($id, [
+            'contain' => [],
+        ]);
+        if ($this->request->is(['patch', 'post', 'put'])) {
+            $review = $this->Reviews->patchEntity($review, $this->request->getData());
+            if ($this->Reviews->save($review)) {
+                $this->Flash->success(__('The review has been saved.'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('The review could not be saved. Please, try again.'));
+        }
+        $reviewers = $this->Reviews->Reviewers->find('list', ['limit' => 200]);
+        $reviewees = $this->Reviews->Reviewees->find('list', ['limit' => 200]);
+        $bidinfos = $this->Reviews->Bidinfos->find('list', ['limit' => 200]);
+        $this->set(compact('review', 'reviewers', 'reviewees', 'bidinfos'));
+    }
+
+    /**
+     * Delete method
+     *
+     * @param string|null $id Review id.
+     * @return \Cake\Http\Response|null Redirects to index.
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function delete($id = null)
+    {
+        $this->request->allowMethod(['post', 'delete']);
+        $review = $this->Reviews->get($id);
+        if ($this->Reviews->delete($review)) {
+            $this->Flash->success(__('The review has been deleted.'));
+        } else {
+            $this->Flash->error(__('The review could not be deleted. Please, try again.'));
+        }
+
+        return $this->redirect(['action' => 'index']);
+    }
+}
