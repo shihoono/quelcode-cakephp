6ecaf9f3c14f9784be8fdfacf5c401245e0e55ef
shihoono <shiho1449@gmail.com>
2020-11-12 14:57:23 +0900

    Reviewsテーブルのマイグレーションファイル作成

---
+++ html/mycakeapp/config/Migrations/20201112055152_CreateReviews.php
@@ -0,0 +1,47 @@
+<?php
+use Migrations\AbstractMigration;
+
+class CreateReviews extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('reviews');
+        $table->addColumn('reviewer_id', 'integer', [
+            'default' => null,
+            'limit' => 11,
+            'null' => false,
+        ]);
+        $table->addColumn('reviewee_id', 'integer', [
+            'default' => null,
+            'limit' => 11,
+            'null' => false,
+        ]);
+        $table->addColumn('bidinfo_id', 'integer', [
+            'default' => null,
+            'limit' => 11,
+            'null' => false,
+        ]);
+        $table->addColumn('rank', 'integer', [
+            'default' => null,
+            'limit' => 1,
+            'null' => false,
+        ]);
+        $table->addColumn('comment', 'string', [
+            'default' => null,
+            'limit' => 1000,
+            'null' => false,
+        ]);
+        $table->addColumn('created', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->create();
+    }
+}
