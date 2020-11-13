cecfdb555b41ed8d36473ec563089ed4491f0178
shihoono <shiho1449@gmail.com>
2020-11-13 20:35:47 +0900

    評価登録の処理

--- html/mycakeapp/src/Controller/ReviewsController.php
+++ html/mycakeapp/src/Controller/ReviewsController.php
@@ -10,22 +10,8 @@ use App\Controller\AppController;
  *
  * @method \App\Model\Entity\Review[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
  */
-class ReviewsController extends AppController
+class ReviewsController extends AuctionController
 {
-    /**
-     * Index method
-     *
-     * @return \Cake\Http\Response|null
-     */
-    public function index()
-    {
-        $this->paginate = [
-            'contain' => ['Reviewers', 'Reviewees', 'Bidinfos'],
-        ];
-        $reviews = $this->paginate($this->Reviews);
-
-        $this->set(compact('reviews'));
-    }
 
     /**
      * View method
@@ -34,82 +20,50 @@ class ReviewsController extends AppController
      * @return \Cake\Http\Response|null
      * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
      */
-    public function view($id = null)
-    {
-        $review = $this->Reviews->get($id, [
-            'contain' => ['Reviewers', 'Reviewees', 'Bidinfos'],
-        ]);
+    // public function view($id = null)
+    // {
+    //     $review = $this->Reviews->get($id, [
+    //         'contain' => ['Users', 'Bidinfo'],
+    //     ]);
 
-        $this->set('review', $review);
-    }
+    //     $this->set('review', $review);
+    // }
 
     /**
      * Add method
      *
      * @return \Cake\Http\Response|null Redirects on successful add, renders view otherwise.
      */
-    public function add()
+    public function add($bidinfo_id = null)
     {
         $review = $this->Reviews->newEntity();
+
+        $bidinfo = $this->Bidinfo->get($bidinfo_id, ['contain'=>['Biditems']]);
+
         if ($this->request->is('post')) {
             $review = $this->Reviews->patchEntity($review, $this->request->getData());
-            if ($this->Reviews->save($review)) {
-                $this->Flash->success(__('The review has been saved.'));
-
-                return $this->redirect(['action' => 'index']);
+            $review->reviewer_id = $this->Auth->user('id');
+            //ログイン者が落札者
+            if($this->Auth->user('id') === $bidinfo->user_id){
+                $review->reviewee_id = $bidinfo->biditem->user_id;
             }
-            $this->Flash->error(__('The review could not be saved. Please, try again.'));
-        }
-        $reviewers = $this->Reviews->Reviewers->find('list', ['limit' => 200]);
-        $reviewees = $this->Reviews->Reviewees->find('list', ['limit' => 200]);
-        $bidinfos = $this->Reviews->Bidinfos->find('list', ['limit' => 200]);
-        $this->set(compact('review', 'reviewers', 'reviewees', 'bidinfos'));
-    }
-
-    /**
-     * Edit method
-     *
-     * @param string|null $id Review id.
-     * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function edit($id = null)
-    {
-        $review = $this->Reviews->get($id, [
-            'contain' => [],
-        ]);
-        if ($this->request->is(['patch', 'post', 'put'])) {
-            $review = $this->Reviews->patchEntity($review, $this->request->getData());
-            if ($this->Reviews->save($review)) {
-                $this->Flash->success(__('The review has been saved.'));
-
-                return $this->redirect(['action' => 'index']);
+            //ログイン者が出品者
+            if($this->Auth->user('id') === $bidinfo->biditem->user_id){
+                $review->reviewee_id = $bidinfo->user_id;
             }
-            $this->Flash->error(__('The review could not be saved. Please, try again.'));
+            $review->bidinfo_id = $bidinfo->id;
+
+            if ($this->Reviews->save($review)) {
+                $this->Flash->success(__('評価しました。'));
+
+                return $this->redirect(['action' => 'add', $bidinfo->id]);
+            }
+            $this->Flash->error(__('保存に失敗しました。もう一度入力下さい。'));
         }
-        $reviewers = $this->Reviews->Reviewers->find('list', ['limit' => 200]);
-        $reviewees = $this->Reviews->Reviewees->find('list', ['limit' => 200]);
-        $bidinfos = $this->Reviews->Bidinfos->find('list', ['limit' => 200]);
-        $this->set(compact('review', 'reviewers', 'reviewees', 'bidinfos'));
+        //評価済みかどうか判定
+        $reviewed = $this->Reviews->find()->where(['bidinfo_id' => $bidinfo_id, 'reviewer_id' => $this->Auth->user('id')])->count();
+
+        $this->set(compact('review', 'bidinfo','reviewed'));
     }
 
-    /**
-     * Delete method
-     *
-     * @param string|null $id Review id.
-     * @return \Cake\Http\Response|null Redirects to index.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function delete($id = null)
-    {
-        $this->request->allowMethod(['post', 'delete']);
-        $review = $this->Reviews->get($id);
-        if ($this->Reviews->delete($review)) {
-            $this->Flash->success(__('The review has been deleted.'));
-        } else {
-            $this->Flash->error(__('The review could not be deleted. Please, try again.'));
-        }
-
-        return $this->redirect(['action' => 'index']);
-    }
 }
