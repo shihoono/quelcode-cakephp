22ba9b8247d3c7c589bb72140cb1ac49c02bd13b
shihoono <shiho1449@gmail.com>
2020-11-25 13:50:43 +0900

    カウントダウン処理　追加

---
+++ html/mycakeapp/webroot/js/countdown.js
@@ -0,0 +1,31 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+  const endTime = new Date(endtime);
+
+  countdown = () => {
+    const now = new Date();
+    const remainingTime = endTime - now;
+    const sec = Math.floor(remainingTime / 1000 % 60);
+    const min = Math.floor(remainingTime / 1000 / 60) % 60;
+    const hours = Math.floor(remainingTime / 1000 / 60 / 60) % 24;
+    const days = Math.floor(remainingTime / 1000 / 60 / 60 / 24);
+    const count = [days, hours, min, sec]; 
+
+  };
+
+  perSecond = () => {
+    setTimeout(function(){
+      update();
+    }, 1000);
+  };
+  
+  update = () => {
+    const counter = countdown();
+    const showCountdown = '残り' + counter[0] + '日' + counter[1] + '時間' + counter[2] + '分' + counter[3] + '秒';
+    document.getElementById('countdown').textContent = showCountdown;
+    perSecond();
+  };
+  
+  update();
+
+}, false); 
